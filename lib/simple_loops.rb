b73695ce777de6f62615c769cd0f3de9d539c85b
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 23:05:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,7 +8,13 @@ def loop_message_five_times(string)
   end
 end
 
-
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
 
 def output_array(array)
   count = 0
